55bb5deec378c335dbb578272b0e3ce3c744d3ff
Sean Holtshousen <holtshousens@gmail.com>
2016-10-25 22:40:49 +0100

    Latest Changes

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using Saturn.DAL.DataContext;
+using Saturn.DAL.DataObjects;
+using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var merchant = new Merchant
+            {
+                MerchantId = 1,
+                MerchantName = "Tesco"
+            };
+
+            using (var context = new TransactionContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Merchants.Add(merchant);
+                context.SaveChanges();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
