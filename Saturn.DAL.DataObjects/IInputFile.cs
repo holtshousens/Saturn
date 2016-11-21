9b218d8b0b40766703505c67ac8cde4cdedad304
Sean Holtshousen <holtshousens@gmail.com>
2016-11-21 22:27:54 +0000

    cleanup

--- Saturn.DAL.DataObjects/IInputFile.cs
+++ Saturn.DAL.DataObjects/IInputFile.cs
@@ -6,11 +6,7 @@ namespace Saturn.DAL.DataObjects
     {
         DateTime TransactionDate { get; set; }
         string TransactionName { get; set; } 
-        //public string CardType { get; set; }
-        //public string CardUser { get; set; }
         string TransactionGroup { get; set; } 
-        //public double AmountCr { get; set; } 
-        //public double AmountDr { get; set; } 
         DateTime TransactionLoadDateTime { get; set; }
     }
 }
